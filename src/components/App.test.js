12b913bac509d634c1653998810b7525a3c7ee33
Dave Aspinall <dave.aspinall@codecomputerlove.com>
2019-05-22 22:27:53 +0100

    chore: add failing test to App component

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -4,9 +4,27 @@ import App from './App'
 
 const wrap = (props = {}) => shallow(<App {...props} />)
 
+const mockHotels = [
+  {
+    name: 'hotelone',
+    starRating: 5,
+    facilities: ['car park', 'pool'],
+  },
+  {
+    name: 'hoteltwo',
+    starRating: 3,
+    facilities: ['car park', 'gym'],
+  },
+  {
+    name: 'hotelthree',
+    starRating: 3,
+    facilities: [],
+  },
+]
+
 describe('App component', () => {
-  it('Renders "Hello World" by default', () => {
-    const wrapper = wrap()
-    expect(wrapper.contains('Hello World')).toBe(true)
+  it('Displays hotels when passed in', () => {
+    const wrapper = wrap({ hotels: mockHotels })
+    expect(wrapper.find('.hotel-card')).toHaveLength(3)
   })
 })
