3b4452d86952794322eec2a2ba9d41e23ae5a068
Dave Aspinall <dave.aspinall@codecomputerlove.com>
2019-05-22 23:28:58 +0100

    chore: add failing tests around the facilities filter

--- private/cypress/integration/home.spec.js
+++ private/cypress/integration/home.spec.js
@@ -1,9 +1,21 @@
 describe('Home Page', () => {
   beforeEach(() => {
     cy.visit('/')
+
+    cy.get('[data-test="hotel-card"]').as('hotels')
   })
 
   it('Displays three hotels by default', () => {
-    cy.get('[data-test="hotel-card"]').should('have.length', 3)
+    cy.get('@hotels').should('have.length', 3)
+  })
+
+  it('Allows a user to filter hotels by a single facility', () => {
+    cy.get('input[name="facilities"]').check(['car park'])
+    cy.get('@hotels').should('have.length', 2)
+  })
+
+  it('Allows a user to filter hotels by multiple facilities', () => {
+    cy.get('input[name="facilities"]').check(['gym', 'car park'])
+    cy.get('@hotels').should('have.length', 1)
   })
 })
