72fbbced36c0e00db703225286e12c61f24227f9
Dave Aspinall <dave.aspinall@codecomputerlove.com>
2019-05-27 16:24:31 +0100

    test: expand acceptance tests to validate edge cases

--- private/cypress/integration/home.spec.js
+++ private/cypress/integration/home.spec.js
@@ -19,4 +19,22 @@ describe('Home Page', () => {
     cy.get('label[for="gym-filter"]').click()
     cy.get('@hotels').should('have.length', 1)
   })
+
+  it('Allows a user to remove filters once they have been selected', () => {
+    cy.get('label[for="car-park-filter"]').click()
+    cy.get('label[for="gym-filter"]').click()
+    cy.get('@hotels').should('have.length', 1)
+
+    cy.get('label[for="gym-filter"]').click()
+    cy.get('@hotels').should('have.length', 2)
+  })
+
+  it('Displays an error message if no hotels matcxh the criteria', () => {
+    cy.get('label[for="car-park-filter"]').click()
+    cy.get('label[for="pool-filter"]').click()
+    cy.get('label[for="gym-filter"]').click()
+
+    cy.get('@hotels').should('have.length', 0)
+    cy.get('[data-test="error-message"]').should('be.visible')
+  })
 })
